ace7aa07f5d2064cbd5612e4bc7d54fd96ad32f1
nipunthathsara <nipunthathsara@gmail.com>
2017-08-03 11:16:31 +0530

    changes in APIMigrationEncryptionServiceComponent class

--- src/main/java/org/wso2/carbon/api/migration/encryption/APIMigrationEncryptionServiceComponent.java
+++ src/main/java/org/wso2/carbon/api/migration/encryption/APIMigrationEncryptionServiceComponent.java
@@ -1,10 +1,14 @@
 package org.wso2.carbon.api.migration.encryption;
 
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /*
 
  */
 public class APIMigrationEncryptionServiceComponent {
 
+    public static final Log log = LogFactory.getLog(APIMigrationEncryptionServiceComponent.class);
 
 }
